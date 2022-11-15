56320cce647e20f673c5f361ff5b7e387f465ec5
Ville Lohkovuori <villelohkovuori1@gmail.com>
2022-11-15 20:28:15 +0200

    remove text highlighting from Letters

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -16,8 +16,8 @@ export default function Letter({ letterObj, handleClick }: LetterProps) {
 
   return (
     <div className="bg-[#d0bf7d] w-[41px] h-[45px] relative rounded-lg shadow-2xl" onClick={() => click()}>
-      <h1 className="absolute top-[15px] left-1/3 w-fit h-fit leading-3 text-lg cursor-default">{letterObj.char}</h1>
-      <h2 className="absolute bottom-0.5 right-0.5 text-sm cursor-default">{letterObj.value}</h2>
+      <h1 className="absolute top-[15px] left-1/3 w-fit h-fit leading-3 text-lg cursor-default select-none">{letterObj.char}</h1>
+      <h2 className="absolute bottom-0.5 right-0.5 text-sm cursor-default select-none">{letterObj.value}</h2>
     </div>
   );
 };
